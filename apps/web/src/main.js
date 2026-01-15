9443f54ed77e1d4f702ae33436e1ef7f2b123e26
joshegilbert <jgilb811@gmail.com>
2026-01-15 09:16:08 -0700

    forgot to save this file

--- apps/web/src/main.js
+++ apps/web/src/main.js
@@ -2,12 +2,23 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import { useAuthStore } from "./stores/auth";
 
 import "./style.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+app.use(pinia);
+
 app.use(router);
 
 app.mount("#app");
+
+const auth = useAuthStore(pinia);
+
+if (auth.token) {
+  auth.fetchMe().catch((err) => {
+    console.warn("Session restore failed", err);
+  });
+}
